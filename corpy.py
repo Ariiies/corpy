fefd60a6eb711a03e3f31788bf00ee50c4ea9ac4
Aries <luisaries1998@gmail.com>
2024-10-22 15:41:22 -0700

    corpy v2.0: improves in Data and Data2 w/ enummerate

--- corpy.py
+++ corpy.py
@@ -25,32 +25,28 @@ class Corpy:                  # Iniciliza el objeto con una lista de Strings
     # bag of words, all in json format
     @property # diccionario sobre las palabras en los documentos, similar a una bag of words en json
     def Data(self):
-        data={}
-        for ele in self.__vocabulary:
-            data[ele], total, ndoc = {}, 0, 0
-            for i in range(1, len(self.__corpus)+1):
-                flag, count, text = True, 0, self.__corpus[i-1].split()
-                for word in text:
-                    if ele == word:
-                        count+=1
-                        total+=1
-                        flag = False
-                        data[ele].update({f'doc{i}': count}) 
-                if flag:
-                    data[ele].update({f'doc{i}': 0})
-            data[ele].update({f'total': total})
-            for l in range(1, len(self.__corpus)+1): 
-                if data[ele][f'doc{l}'] != 0:
-                    ndoc +=1
-            data[ele].update({f'exist in': ndoc})               
+        data = {}
+        for word in self.__vocabulary:
+            word_data, total, count_doc = {}, 0, 0
+            for i, text in enumerate(self.__corpus, start=1):
+                count = text.split().count(word)
+                total += count
+                word_data[f'doc{i}'] = count
+                if count > 0:
+                    count_doc += 1
+            word_data['total'] = total
+            word_data['exist in'] = count_doc
+            data[word] = word_data
+            
         return data
    
     # Anothes function for the general data in the docs 
     @property # datos de la cantidad de palabras en los documentos
     def Data2(self):
-        data, total={},0
-        for doc in range(len(self.__corpus)):
-            total+= len(self.__corpus[doc].split())
-            data[f"doc{doc+1}"]= {f"total": len(self.__corpus[doc].split())}
-        data["all"]={"total":total}
+        data, total = {}, 0
+        for i, text in enumerate(self.__corpus, start=1):
+            word_count = len(text.split())
+            total += word_count
+            data[f"doc{i}"] = {"total": word_count}
+        data["all"] = {"total": total}
         return data
